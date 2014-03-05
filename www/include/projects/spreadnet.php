6e0ada966cc199376e787dca23c4510155937f90
Id <idmarjr@gmail.com>
2014-03-05 17:22:54 -0300

    Now each project is in their own include.

---
+++ www/include/projects/spreadnet.php
@@ -0,0 +1,14 @@
+<section class="project spreadnet">
+	<header>
+		<h1>SPREADnet</h1>
+		<strong>Web app</strong>
+	</header>
+	<div class="description">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div><!-- End .description -->
+	<div class="illustrate">
+		<figure>
+			<img src="img/projects/SPREADnet.jpg" alt="SPREADnet screenshot" />
+		</figure>
+	</div><!-- End .illustrate -->
+</section><!-- End .project.spreadnet -->
---
+++ www/include/projects/wayTwo.php
@@ -0,0 +1,14 @@
+<section class="project wayTwo">
+	<header>
+		<h1>Way2 Display</h1>
+		<strong>Web App</strong>
+	</header>
+	<div class="description">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div><!-- End .description -->
+	<div class="illustrate">
+		<figure>
+			<img src="img/projects/way2.jpg" alt="Way2 Display screenshot" />
+		</figure>
+	</div><!-- End .illustrate -->
+</section><!-- End .project.wayTwo -->
