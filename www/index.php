e2e8aa190f9d0fe884c398189407a8ffa9dd2ddb
Id <idmarjr@gmail.com>
2014-03-05 03:03:20 -0300

    Include necessary fonts from Google Webfonts

--- www/index.php
+++ www/index.php
@@ -20,7 +20,8 @@
 	<link rel="stylesheet" media="screen" href="css/app.css"/>
 
 	<!-- Web Fonts -->
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic|Roboto+Condensed:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
 	<!-- Standart Favicon -->
 	<link rel="shortcut icon" type="image/png" href="img/deviceIcons/favicon.png"/>
