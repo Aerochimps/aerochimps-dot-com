8db54ad5734733fc730b6394e41939a72db242aa
Id <idmarjr@gmail.com>
2014-03-05 03:07:20 -0300

    Moving the <nav> to out of .mainHeader

--- www/include/mainHeader.php
+++ www/include/mainHeader.php
@@ -1,14 +1,20 @@
 <header class="mainHeader">
-	<h1>Aerochimps</h1>
-	<nav>
-		<ul>
-			<li><a href="">Chimps</a></li>
-			<li><a href="">Time</a></li>
-			<li><a href="">Projetos</a></li>
-			<li><a href="">Clientes</a></li>
-			<li><a href="">Contato</a></li>
-			<li class="hide"><a href="">Blog</a></li>
-		</ul>
-	</nav>
-	<span class="hideNav">Esconder o menu</span>
+	<h1>
+		<span class="vhide">Aerochimps</span>
+		<img src="img/logos/logo-name-a19c91.svg" alt="Aerochimps Loho"/>
+	</h1>
+	<div class="viewNav">
+		<img src="img/menuIcon.svg" alt="Mostrar/Esconder menu"/>
+		<span class="vhide">Mostrar/Esconder o menu</span>
+	</div><!-- End .viewNav -->
 </header> <!-- End .mainHeader -->
+<nav class="hide">
+	<ul>
+		<li><a href="">Chimps</a></li>
+		<li><a href="">Time</a></li>
+		<li><a href="">Projetos</a></li>
+		<li><a href="">Clientes</a></li>
+		<li><a href="">Contato</a></li>
+		<li class="hide"><a href="">Blog</a></li>
+	</ul>
+</nav>
