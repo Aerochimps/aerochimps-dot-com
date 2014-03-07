eb26be7c6fbdb46134f688917908547f85db9dc8
Id <idmarjr@gmail.com>
2014-03-07 20:34:50 -0300

    .mainNav
    - Basic styles;
    - Basic functionality;

--- www/js/frontend.js
+++ www/js/frontend.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+	$('.viewNav').click(function(){
+		$('.mainNav').toggleClass('close');
+	});
+
+	$('.mainNav li a').click(function(){
+		$(this).closest('.mainNav').toggleClass('close');
+	});
+
 	$('.project .description').expander({
 		slicePoint: 300,
 		expandText: 'ver mais',
