7a457256cd72f56376fb069448a6ccca2db1f0ed
Id <idmarjr@gmail.com>
2014-03-13 20:31:07 -0300

    #19
    Code is there! Now we need to decide what it will gonna do.

--- www/js/frontend.js
+++ www/js/frontend.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
 
-	//////////////////////////////////
+	////////////////////////////////////////////////////
+	//Konami Code
+	////////////////////////////////////////////////////
+	$.fn.konami = function(callback, code) {
+		if(code == undefined) code = "38,38,40,40,37,39,37,39,66,65"
+		return this.each(function() {
+			var kkeys = [];
+			$(this).keydown(function(e){
+				kkeys.push( e.keyCode );
+				if (kkeys.toString().indexOf( code ) >= 0 ){
+					$(this).unbind('keydown', arguments.callee);
+					callback(e);
+				}
+			});
+		});
+	}
+	$(window).konami(function(){
+		//Do something Cool! :D
+	})
+
+	////////////////////////////////////////////////////
 	//Show/Hide .mainNav
-	//////////////////////////////////
+	////////////////////////////////////////////////////
 	$('.viewNav').click(function(){
 		$('.mainNav').toggleClass('close');
 	});
@@ -14,9 +34,9 @@ $(document).ready(function() {
 		$(this).addClass('active');
 	});
 
-	//////////////////////////////////
+	////////////////////////////////////////////////////
 	//Scroll to each section
-	//////////////////////////////////
+	////////////////////////////////////////////////////
 	$('.sectionChimps').click(function(){
 		$('.chimps').animatescroll({scrollSpeed:2000});
 	});
@@ -37,9 +57,9 @@ $(document).ready(function() {
 		$('.contact').animatescroll({scrollSpeed:2000});
 	});
 
-	//////////////////////////////////
+	////////////////////////////////////////////////////
 	//Read more in .project
-	//////////////////////////////////
+	////////////////////////////////////////////////////
 	$('.project .description').expander({
 		slicePoint: 300,
 		expandText: 'ver mais',
