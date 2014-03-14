b77ce05b5e22c9549df1d2515bd208d788342106
Id <idmarjr@gmail.com>
2014-03-14 00:34:05 -0300

    #11
    - Formatting Feedback styles for error input;
    - Formatting Feedback styles for error messages;
    - Hide .contactFeedback by default;
    
    @gumaestri …Still ned to fix some behaviors.

--- www/include/contact.php
+++ www/include/contact.php
@@ -1,6 +1,6 @@
 <section id="contact" class="contact">
 	<p class="intro">Agora que você já nos conhece melhor, vamos conversar? Entre em <strong>contato</strong> com a gente.</p>
-	<p id="contactFeedback"></p>
+	<p id="contactFeedback" class="contactFeedback hide">Obrigado pelo seu contato!</p>
 	<form id="contactForm" class="contactForm" method="POST" action="contactForm.php">
 		<div class="field">
 			<label class="vhide">Nome:</label>
