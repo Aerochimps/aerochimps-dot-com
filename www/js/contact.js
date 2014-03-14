388ab0b220a773cfb2f6fee11b7d139f56d33e44
Id <idmarjr@gmail.com>
2014-03-13 22:13:41 -0300

    indentation

--- www/js/contact.js
+++ www/js/contact.js
@@ -1,30 +1,29 @@
 $(function() {
-  $("#contactForm").validate({
-	  rules: {
-	    name: {
-	      required: true
-	    },
-	    email:{
-	    	required: true,
-	    	email: true
-	    },
-	    subject:{
-	    	required: true
-	    },
-	    message:{
-	    	required: true
-	    }
-	  },
-	  submitHandler: function(form) {
-	    $.ajax({
-		  type: "POST",
-		  url: "contact.php",
-		  data: $(form).serialize(),
-		  success: function(data, textStatus, jqXH){
-		  	
-		  		$("#contactFeedback").text(data);
+	$("#contactForm").validate({
+		rules: {
+			name: {
+				required: true
+			},
+			email:{
+				required: true,
+				email: true
+			},
+			subject:{
+				required: true
+			},
+			message:{
+				required: true
 			}
-	  	});
-	  }
-  });
+		},
+		submitHandler: function(form) {
+			$.ajax({
+				type: "POST",
+				url: "contact.php",
+				data: $(form).serialize(),
+				success: function(data, textStatus, jqXH){
+					$("#contactFeedback").text(data);
+				}
+			});
+		}
+	});
 });
