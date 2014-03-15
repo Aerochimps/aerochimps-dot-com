cd0fd4f1f7cd0204fa01cc8fe44c5aaec635047f
Id <idmarjr@gmail.com>
2014-03-15 18:02:48 -0300

    Moving files out of the bower control to js folder.
    Doing it to avoid missing files when using bower install

--- www/index.php
+++ www/index.php
@@ -72,10 +72,10 @@
 	</section> <!-- End .main -->
 
 	<!-- Scripts -->
-	<script src="bower_components/cssua/cssua.min.js"></script>
+	<script src="js/cssua.min.js"></script>
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 	<script src="bower_components/animatescroll/animatescroll.js"></script>
-	<script src="bower_components/jquery-parallax/jquery.parallax.js"></script>
+	<script src="js/jquery.parallax.js"></script>
 	<script src="bower_components/jquery.expander/jquery.expander.min.js"></script>
 	<script src="bower_components/jquery-validation/jquery.validate.js"></script>
 	<script src="js/frontend.js"></script>
