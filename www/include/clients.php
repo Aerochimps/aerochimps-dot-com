7f26a6e2b2c1df26e1a3a153cbef285ba249a9ac
Id <idmarjr@gmail.com>
2014-03-16 16:38:42 -0300

    #6
    - Adjust SVG logos;
    - Delete old .png placeholders;

--- www/include/clients.php
+++ www/include/clients.php
@@ -1,13 +1,13 @@
 <section id="clients" class="clients">
 	<p class="intro">Com <strong>clientes</strong> como esses, é fácil amar o que se faz.</p>
 	<ul class="cf">
-		<li><a href="#" target="_blank"><img src="img/clients/aws.png" alt="Amazon Web Services" /></a></li>
-		<li><a href="#" target="_blank"><img src="img/clients/astrea.png" alt="Astrea" /></a></li>
-		<li><a href="#" target="_blank"><img src="img/clients/compedi.png" alt="Compedi" /></a></li>
+		<li><a href="#" target="_blank"><img src="img/clients/aws.svg" alt="Amazon Web Services" /></a></li>
+		<li><a href="#" target="_blank"><img src="img/clients/astrea.svg" alt="Astrea" /></a></li>
+		<li><a href="#" target="_blank"><img src="img/clients/compedi.svg" alt="Compedi" /></a></li>
 		<li><a href="#" target="_blank"><img src="img/clients/coolendar.png" alt="Coolendar" /></a></li>
-		<li><a href="#" target="_blank"><img src="img/clients/sebrae.png" alt="Sebrae" /></a></li>
-		<li><a href="#" target="_blank"><img src="img/clients/spreadnet.png" alt="SPREADnet" /></a></li>
-		<li><a href="#" target="_blank"><img src="img/clients/videoface.png" alt="Videoface" /></a></li>
+		<li><a href="#" target="_blank"><img src="img/clients/sebrae.svg" alt="Sebrae" /></a></li>
+		<li><a href="#" target="_blank"><img src="img/clients/spreadnet.svg" alt="SPREADnet" /></a></li>
+		<li><a href="#" target="_blank"><img src="img/clients/videoface.svg" alt="Videoface" /></a></li>
 		<li><a href="#" target="_blank"><img src="img/clients/way2.png" alt="Way2" /></a></li>
 	</ul>
 </section> <!-- End .clients -->
