06a504cbc8352fd82f933045118d00e7c7d5db2d
Id <idmarjr@gmail.com>
2014-03-31 19:36:17 -0300

    Disable Zoon in mobile devices

--- www/index.php
+++ www/index.php
@@ -9,7 +9,7 @@
 	<![endif]-->
 
 	<!-- Metas -->
-	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	<meta name="description" content=""/>
 	<meta name="author" content="Aerochimps"/>
 	<meta name="rating" content="general"/>
