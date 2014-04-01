f5456bb979ea0b680d3fcbfcab2e9fde78d1890b
gumaestri <gumaestri@gmail.com>
2014-04-01 20:27:08 -0300

    fix contact form , and set facebook to correct path

--- www/include/contactForm.php
+++ www/include/contactForm.php
@@ -1,4 +1,6 @@
 <?php
+    $mailcopyfile = '/home/aerochimp/aerochimps.com/email_copy.txt';
+
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
@@ -8,11 +10,15 @@
     $body = $message;
 
     $headers = "";
-    $headers .= "From: ". $name ." <". $email .">\n";
+    $headers .= "From: ". $name ."\n";
     $headers .= "Reply-To: ". $email ."\n";
-    $headers .= "X-Mailer: PHP/".phpversion();
+    $headers .= "X-Mailer: PHP/".phpversion()."\n";
 
     if (mail ($to, $subject, $body, $headers)) { 
+        $fp = fopen($mailcopyfile, "a"); 
+        $now = date('Y-m-d H:i:s');
+        fputs($fp, $now ."\n". $headers . $subject ."\n". $body . "\n\n");
+        fclose($fp);
         echo 'Obrigado pelo seu contato!';
     } else { 
         echo 'Ops!! Nossos macacos não conseguiram enviar sua mensagem, tente novamente!'; 
