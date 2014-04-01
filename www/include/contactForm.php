0929266f6de39cb27951f859f2829bcc0f36a402
gumaestri <gumaestri@gmail.com>
2014-03-31 23:15:26 -0300

    ajustes no email

--- www/include/contactForm.php
+++ www/include/contactForm.php
@@ -2,9 +2,7 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    // $from = "contact@aerochimps.com"; 
-     $to = 'contact@aerochimps.com'; 
-    // $to = "gumaestri@gmail.com";
+    $to = 'contact@aerochimps.com'; 
     $subject = $_POST['subject'];
 
     $body = $message;
@@ -12,6 +10,7 @@
     $headers = "";
     $headers .= "From: ". $name ." <". $email .">\n";
     $headers .= "Reply-To: ". $email ."\n";
+    $headers .= "X-Mailer: PHP/".phpversion();
 
     if (mail ($to, $subject, $body, $headers)) { 
         echo 'Obrigado pelo seu contato!';
