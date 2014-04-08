d67506fac8f90605ce4251891ca730b540e03078
gumaestri <gumaestri@gmail.com>
2014-04-08 08:57:52 -0300

    remove meta tag language

--- www/index.php
+++ www/index.php
@@ -10,7 +10,7 @@
 
 	<!-- Metas -->
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-	<meta name="language" content="pt-br, en-us" />
+	<!-- <meta name="language" content="pt-br, en-us" /> -->
 	<meta name="description" content="Desenvolvemos aplicativos web e mobile, com soluções criativas e inovadoras, adequadas às necessidades do cliente e focadas no usuário." />
 	<meta name="keywords" content="web development, desenvolvimento web floripa, desenvolvimento de aplicativos floripa, web development brazil, mobile brazil, ux floripa, ux florianópolis, consultoria ux"/>
 	<meta name="author" content="Aerochimps" /> 
