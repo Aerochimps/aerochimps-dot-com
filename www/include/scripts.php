f4efc6a3cb78bb978af9e18f0f93178107c3c08d
Id <idmarjr@gmail.com>
2014-07-27 17:53:37 -0300

    #37
    - Link to Google+ Business site;

--- www/include/scripts.php
+++ www/include/scripts.php
@@ -1,4 +1,4 @@
-	<script src="../../js/cssua.min.js"></script>
+<script src="../../js/cssua.min.js"></script>
 	<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
 	<script src="../../bower_components/animatescroll/animatescroll.js"></script>
 	<script src="../../bower_components/jquery.expander/jquery.expander.min.js"></script>
@@ -6,3 +6,6 @@
 	<script src="../../js/contact.js"></script>
 	<script src="../../js/scroll.js"></script>
 	<script src="../../js/frontend.js"></script>
+
+	<!-- Google+ Business link -->
+	<a href="https://plus.google.com/103080800981844045717" rel="publisher" class="vhide">Google+</a>
