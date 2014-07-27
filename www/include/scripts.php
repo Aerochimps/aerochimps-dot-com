db42439b0f6a91a42d7eeabfb23e5038b38eaafc
Id <idmarjr@gmail.com>
2014-07-27 18:34:21 -0300

    #42
    - Fix Life, the universe and everything else;
    -
    [42](http://en.wikipedia.org/wiki/Phrases_from_The_Hitchhiker%27s_Guide_
    to_the_Galaxy#Answer_to_the_Ultimate_Question_of_Life.2C_the_Universe.2C
    _and_Everything_.2842.29)

--- www/include/scripts.php
+++ www/include/scripts.php
@@ -3,6 +3,7 @@
 	<script src="../../bower_components/animatescroll/animatescroll.js"></script>
 	<script src="../../bower_components/jquery.expander/jquery.expander.min.js"></script>
 	<script src="../../bower_components/jquery-validation/jquery.validate.js"></script>
+	<script src="../../js/jquery.autogrow-textarea.js"></script>
 	<script src="../../js/contact.js"></script>
 	<script src="../../js/scroll.js"></script>
 	<script src="../../js/frontend.js"></script>
