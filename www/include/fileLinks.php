38c31ef6d9568a93b25a723a61a04016fdf0932a
idmarjr <idmarjr@gmail.com>
2014-09-30 22:39:31 -0300

    Pink October customization :)
    http://en.wikipedia.org/wiki/National_Breast_Cancer_Awareness_Month

--- www/include/fileLinks.php
+++ www/include/fileLinks.php
@@ -1,5 +1,6 @@
 <!-- CSS -->
 	<link rel="stylesheet" media="screen" href="../../css/app.css"/>
+	<link rel="stylesheet" media="screen" href="../../css/pinkOctober.css"/>
 	
 	<!-- Web Fonts -->
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
