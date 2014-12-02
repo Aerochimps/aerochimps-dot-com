194fc66da7fe1b806e12f6116e5772b169230916
idmarjr <idmarjr@gmail.com>
2014-12-02 11:22:53 -0200

    Get the site back to original color :)
    - Remove blueNovember.css;
    - Add a underline to .scss “campaign” files;

--- www/include/fileLinks.php
+++ www/include/fileLinks.php
@@ -1,7 +1,6 @@
 <!-- CSS -->
 	<link rel="stylesheet" media="screen" href="../../css/app.css"/>
-	<link rel="stylesheet" media="screen" href="../../css/blueNovember.css"/>
-	
+
 	<!-- Web Fonts -->
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
