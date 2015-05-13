208212c099aa6e92f072fa3113e695fd3879cf73
Idmar Ramos Junior (Id) <idmarjr@gmail.com>
2015-05-12 22:40:55 -0300

    Führer = BAD / Lead = GOOD
    Sorry about that. :(

--- www/lang/de/include/team.php
+++ www/lang/de/include/team.php
@@ -8,7 +8,7 @@
 				</a>
 			</figure>
 			<h3>Marcela Quint</h3>
-			<h4>UX Designer Führer</h4>
+			<h4>Lead UX Designer</h4>
 			<div class="description">
 				<p>B.A. in Grafik-Design an der UDESC , arbeitete bereits in verschiedenen Projekten für User Experience und User Interface Design. Bei Aerochimps ist es Ihre Aufgabe, wunderschöne und freundliche Web- und mobilen Anwendungen zu konzipieren. In ihrer Freizeit , versucht sie mit ihrem  Fahrrad die Welt zu erobern.</p>
 			</div><!-- End .description -->
@@ -21,7 +21,7 @@
 				</a>
 			</figure>
 			<h3>Id<span class="vhide">mar Ramos Junior</span></h3>
-			<h4>Frontend Ingenieur Führer</h4>
+			<h4>Lead Frontend Ingenieur</h4>
 			<div class="description">
 				<p>Experte in Interaction-Design und Frontend-Entwicklung, lebt zwischen Schnittstellen und Code-Zeilen. Er ist ein Meister in HTML und CSS. Bei Aerochimps ist seine Hauptaufgabe die Skalierbarkeit und semantische Integrität unserer Projekte sicherzustellen. In seiner Freizeit genießt er es Podcasts zu hören, mit dem Motorrad unterwegs zu sein und von dem Tag zu träumen, alles fallen zu lassen und und sein Lebensunterhalt durch das spielen von Videospiele zu  bestreiten.</p>
 			</div><!-- End .description -->
@@ -34,7 +34,7 @@
 				</a>
 			</figure>
 			<h3>Gustavo Maestri</h3>
-			<h4>Software Ingenieur Führer</h4>
+			<h4>Lead Software Ingenieur</h4>
 			<div class="description">
 				<p>BS in Informatik an der Unisul, arbeitet seit 13 Jahren in der Software-Entwicklung. In Web-Software spezialisiert ist, dominiert er in Java, Android , JavaScript, Objective C und die Lua Programmiersprache. Seine Rolle bei Aerochimps ist, das Getriebe in Gang zu halten, so sollte alles wie geplant funktioniert. Wenn er nicht gerade Coded, treibt er aktiv Sport und genießt die Zeit mit seiner Familie.</p>
 			</div><!-- End .description -->
