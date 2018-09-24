8940eea647dedf28b2da240ad643c41f50053bb1
Idmar Ramos Junior (Id) <idmar.ramos@booking.com>
2018-09-24 23:35:28 +0200

    Accessibility improvementes
    - Remove maxmum-scale attribute;
    - Remove user-scalable=0 attribute;

--- www/lang/en/index.php
+++ www/lang/en/index.php
@@ -13,7 +13,7 @@
 	<![endif]-->
 
 	<!-- Metas -->
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 	<meta name="author" content="Aerochimps" />
 	<meta name="rating" content="general" />
 	<meta name="dcterms.rights" content="Aerochimps" />
