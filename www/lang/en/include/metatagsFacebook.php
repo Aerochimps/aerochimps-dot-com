c708d7550bcf642437b4db2b36a5376046982510
Idmar Ramos Junior (Id) <idmar.ramos@booking.com>
2018-09-24 23:09:04 +0200

    og:url -> Fix wrong path to languages DE and EN
    Also use https link instead of only http

--- www/lang/en/include/metatagsFacebook.php
+++ www/lang/en/include/metatagsFacebook.php
@@ -1,4 +1,4 @@
 <meta property="og:site_name" content="Aerochimps - We make apps fly!"/>
 	<meta property="og:title" content="Aerochimps - We make apps fly!"/>
-	<meta property="og:url" content="http://aerochimps.com/lang/eng"/>
+	<meta property="og:url" content="https://aerochimps.com/lang/en"/>
 	<meta property="og:description" content="Web and mobile app development company located in Florianópolis, Brazil. Creative and innovative solutions to clients and users."/>
