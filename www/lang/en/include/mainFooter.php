6f84ec2b6aca630de599962f4c5533aad5306aa0
Idmar Ramos Jr. (Id) <idmarjr@gmail.com>
2021-06-19 16:50:04 +0200

    Remove .thanks block from .mainFooter

--- www/lang/en/include/mainFooter.php
+++ www/lang/en/include/mainFooter.php
@@ -1,8 +1,5 @@
 <footer class="mainFooter">
 	<div class="wrapper">
-		<div class="thanks">
-			<p>Thanks for the visit.<br/>Help us to conquer the world by <a id="facebookShare" href="#">sharing</a> this website.</p>
-		</div><!-- End .thanks -->
 		<img src="../../img/logos/logo-img.svg" alt="Aerochimps logo" />
 	</div><!-- End .wrapper -->
 </footer><!-- End .mainFooter -->
